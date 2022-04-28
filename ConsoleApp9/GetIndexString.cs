2f80c541087f7ca77846690b407843864d3d2626
Виктор Лукашев <lukashev-viktor2012@yandex.ru>
2022-04-28 07:56:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/GetIndexString.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    public class GetIndexString
+    {
+        public static void GetIndex(string str)
+        {
+            
+            int index = 0;
+            int sum = 1;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                int TempSum = 1;
+                for (int j = i + 1; j < str.Length; j++)
+                {
+                    if (str[i] == str[j])
+                    {
+                        TempSum++;
+                    }
+                    else
+                    {
+
+                        if (sum < TempSum)
+                        {
+                            sum = TempSum;
+                            index = i;
+                          
+
+                        }
+
+                        i = j - 1;
+                        break;
+
+
+
+
+
+
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"индекс - {index}  кол-во символов - {sum}");
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string str = "aaannaaaccbbbccchhhhhhhhhhhccbbnb";
+            GetIndexString.GetIndex(str);
+            Console.ReadLine();
+
+        }
+    }
+}
+
+       
+ 
